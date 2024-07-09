ecdde5507de3a33695ed8b8d398276915c63b1b3
Sergio Giro <sergio.giro.ar@gmail.com>
2024-07-09 17:18:40 +0100

    Fix jest test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import TodoContainer from "./components/TodoContainer";
+import { store } from './Redux';
+import { Provider } from "react-redux";
 
 
 function App(props: {}) {
@@ -9,7 +11,9 @@ function App(props: {}) {
     <main className="content">
       <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
       <div className="row ">
-        <TodoContainer />
+        <Provider store={store}>
+          <TodoContainer />
+        </Provider>
       </div>
     </main>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,12 @@
   import 'bootstrap/dist/css/bootstrap.min.css';       // add this
   import './index.css';
   import App from './App';
-  import { Provider } from 'react-redux';
   import * as serviceWorker from './serviceWorker';
-  import { store } from './Redux';
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </React.StrictMode>
   );
 
